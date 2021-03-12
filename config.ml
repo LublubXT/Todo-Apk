7c259ffebcce936d3ab1b0830c99b39ed329d9f3
LublubXT <73581388+LublubXT@users.noreply.github.com>
2021-03-12 16:21:55 +0200

    Create config.ml

---
+++ config.ml
@@ -0,0 +1,10 @@
+<?xml version='1.0' encoding='utf-8'?>
+<widget id="my.first.app" version="1.0.0">
+  <name>Todo</name>
+  <description>
+    A Todo list app
+  </description>
+  <author email="aztecinc2021@gmail.com" href="https://azitecteam.herokuapp.com">
+    Azi Tec Team
+  </author>
+</widget>
